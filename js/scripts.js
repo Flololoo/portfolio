2fab38d2b26e209f6b25320f89037ab1754e9462
Flo Muellner <florian.muellner95@gmail.com>
2020-04-23 16:02:32 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+import './bootstrap.min.js';
+
+$(document).ready(function () {
+
+    console.log('log')
+
+    document.addEventListener('scroll', function (event) {
+    }, true /*Capture event*/);
+});
